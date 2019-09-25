3946b9a079a518f38c5ea31d491c8aeb66398428
hraya <hraya@lssu.edu>
2019-09-24 20:43:18 -0500

    Completed lesson-1

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import http from 'http'
 import { createServer } from 'http'
 
 import app from './server'
+const server = http.createServer(app)
 let currentApp = app
 
+
+
+server.listen(1234, () => {
+	console.log(`Live on PORT 1234`)
+})
 
 if (module.hot) {
 	module.hot.accept(['./server'], () => {
--- src/server.js
+++ src/server.js
@@ -4,6 +4,24 @@ import { restRouter } from './api'
 import { connect } from './db'
 import { signin, protect } from './api/modules/auth'
 // Declare an app from express
-const app = null
+const app = express()
+
+// solution code
+setupMiddware(app)
+connect()
+
+app.use('/signin', signin)
+
+
+// I wrote this below
+app.get('/', (req, res) => {
+    res.json({ok: true})
+})
+
+
+// solution code catch all
+// app.all('*', (req, res) => {
+//     res.json({ok: true})
+// })
 
 export default app
